463d3f662e9cc060493f8b74e6515cdc6d510c24
Gabriel <campi.utn.gabriel@gmail.com>
2021-07-29 15:49:45 -0300

    29.1

--- Formularios/src/app/encabezado/encabezado.component.ts
+++ Formularios/src/app/encabezado/encabezado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../services/usuario.service';
 
 @Component({
   selector: 'app-encabezado',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EncabezadoComponent implements OnInit {
 
-  constructor() { }
+  constructor(public UsuarioInyectado: UsuarioService) { }
 
   ngOnInit(): void {
+    this.UsuarioInyectado.usuario.nombres
+  }
+  cambiarNombre(){
+    this.UsuarioInyectado.usuario.nombres = 'ana'
   }
 
 }
--- Formularios/src/app/perfil/perfil.component.ts
+++ Formularios/src/app/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../services/usuario.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  constructor(public UsuarioInyectado: UsuarioService) { }
 
   ngOnInit(): void {
+  }
+  cambiarNombre(){
+    this.UsuarioInyectado.usuario.nombres= 'carlos'
   }
 
 }
